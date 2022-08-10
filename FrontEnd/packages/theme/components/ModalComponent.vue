e3c69050980dc9d727b607ff9d5447fb822c226c
Sujit Tingare <sujittingare@gmail.com>
2022-08-10 18:59:51 +0530

    removed dropdown of location and fixed stylinge

--- FrontEnd/packages/theme/components/ModalComponent.vue
+++ FrontEnd/packages/theme/components/ModalComponent.vue
@@ -40,7 +40,7 @@
               </SfButton>
             </template>
           </div>
-          <!-- <p class="p1">or</p>
+          <p class="p1">or</p>
           <div class="btn">
             <div>
               <SfButton id="btn" @click="$emit('currentLocation')">
@@ -55,7 +55,7 @@
                 <h5>ENABLE LOCATION</h5>
               </SfButton>
             </div>
-          </div> -->
+          </div>
         </div>
         <template> </template>
 
