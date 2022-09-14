96dcdfcf4344b1b9718fa54dd48aed1f185b4268
Ujjwal <shyamkumartiwari405@gmail.com>
2022-09-14 14:41:20 +0530

    Remove the hard-coded values
    * Removed hard-coded values of pick-up and drop location

--- FrontEnd/packages/theme/pages/Search.vue
+++ FrontEnd/packages/theme/pages/Search.vue
@@ -282,15 +282,9 @@ export default {
       if (noSearchFound.value) noSearchFound.value = false;
       toggleLoadindBar(false);
 
-      /*await search({
-        pickup_location: localStorage.getItem('pickUpLatAndLong'),
-        drop_location: localStorage.getItem('dropLatAndLong')
-      });*/
       await search({
-        pickup_location: '12.903561,77.5939631',
-        // localStorage.getItem('pickUpLatAndLong'),
-        drop_location:  "12.9175403,77.5890075"
-        // localStorage.getItem('dropLatAndLong')
+        pickup_location: localStorage.getItem('pickUpLatAndLong'),
+        drop_location:  localStorage.getItem('dropLatAndLong')
       });
 
       localStorage.setItem(
